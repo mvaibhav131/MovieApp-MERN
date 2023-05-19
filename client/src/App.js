6985e0f79acc8b0736102e76633b70bd5aed8fcd
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-05-20 00:12:23 +0530

    :accept: added routes app and index file

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/header/navigation.js';
+import FilmList from './components/film-list/film-list';
+import Film from './components/film-item/film-item';
+import Searched from './components/searched-list/searched-list';
+import {Switch, Route} from 'react-router-dom';
+import Favorites from './components/favorites/favorites';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation/>
+      <br />
+      <Switch>
+
+        <Route exact path='/'>
+          <FilmList/>
+        </Route>
+
+        <Route exact path='/film/:id'>
+          <Film/>
+        </Route>
+
+        <Route exact path='/search_results/:query'>
+          <Searched/>
+        </Route>
+
+        <Route exact path='/favorites'>
+          <Favorites/>
+        </Route>
+
+      </Switch>
     </div>
   );
 }
