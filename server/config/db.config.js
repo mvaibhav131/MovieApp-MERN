77a66200f04e181308360ba331973ad4493f7e94
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-05-19 19:11:39 +0530

    :+1: database connected

---
+++ server/config/db.config.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/MovieDb',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+  })
+  .then(() => {
+      console.log(
+        "Connected to database on port 8000 and database MovieDb!"
+      );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
